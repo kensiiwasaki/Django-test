c2173c835dd3b0dbb8a2b37bc75f49e0a396ea0f
けんし <kensiiwasaki@gmail.com>
2023-01-03 20:43:04 +0900

    add: Modelクラスとマイグレーションの学習

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+ 
 # Create your models here.
+class item(models.Model):          # 追加
+    code = models.IntegerField()   # 追加
+    name = models.TextField()      # 追加
+    price = models.IntegerField()  # 追加
